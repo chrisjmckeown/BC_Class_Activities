eb7e2169e6447fbf8f07cec240521411dcac9a6e
Chris Mckeown <chris.j.mckeown@hotmail.com>
2020-10-26 18:43:29 +1100

    task 01

--- Week 13/1.Handlebars/01-ExpressCalculator/Unsolved/expressCalculator.js
+++ Week 13/1.Handlebars/01-ExpressCalculator/Unsolved/expressCalculator.js
@@ -1,52 +1,36 @@
-// Dependencies
-var express = require("express");
+const express = require("express");
+const PORT = process.env.PORT || 8080;
+const app = express();
 
-// Set the port of our application
-// process.env.PORT lets the port be set by Heroku
-var PORT = process.env.PORT || 8080;
+app.get("/:num1/:operation/:num2", function (req, res) {
+  const num1 = parseInt(req.params.num1);
+  const operation = req.params.operator;
+  const num2 = parseInt(req.params.num2);
 
-// Create express app instance.
-var app = express();
-
-// Routes
-// What routes do you need to have? Which ones are optional?
-// TODO Add your routes here
-app.get("", function(req, res) {
-
-  // TODO parse out the variables from the request
-  // Parameters are received from the URL
-  // TODO make sure they're converted to integers (and not strings)
-  // Parameters are converted to integers
-
-  // Initialize the result variable to send later
-  var result;
-  // Switch statement chooses operation based on the operation parameter.
+  let result;
   switch (operation) {
-  // BONUS - How could you use * + etc. inside the app.get()?
-  case "add":
-    // Add your logic here. Pun intended.
-    break;
-  case "subtract":
-    // Subtract logic
-    break;
-  case "multiply":
-    // Multiply
-    break;
-  case "divide":
-    // Divide
-    break;
-  default:
-    // Handle anything that isn't specified
-    result = "Sorry! The only valid operations are add, subtract, multiply, and divide.";
+    case "add":
+    case "+":
+      result = num1 + num2;
+      break;
+    case "subtract":
+    case "-":
+      result = num1 - num2;
+      break;
+    case "multiply":
+    case "*":
+      result = num1 * num2;
+      break;
+    case "divide":
+    case "/": // %2F
+      result = num1 / num2;
+      break;
+    default:
+      result = "Sorry! The only valid operations are add, subtract, multiply, and divide.";
   }
-
-  // We return the result back to the user in the form of a string
   res.send(result.toString());
-
 });
 
-// Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
+app.listen(PORT, () => {
   console.log("Server listening on: http://localhost:" + PORT);
 });
